f67fb3e402fa298cea896531cdef72c72e7780cd
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-07-24 15:57:53 +0100

    mixins, generics in drf

--- core/views.py
+++ core/views.py
@@ -2,13 +2,14 @@ from django.contrib.auth.models import Permission
 from django.http.response import JsonResponse
 from django.shortcuts import render
 
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated 
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
 
 from .serializers import PostSerializer
 from .models import Post
-
 
 class TestView(APIView):
 
@@ -25,3 +26,30 @@ class TestView(APIView):
 			serializer.save()
 			return Response(serializer.data)
 		return Response(serializer.errors)
+
+class PostView(
+	mixins.ListModelMixin, 
+	mixins.CreateModelMixin,
+	generics.GenericAPIView):
+
+	serializer_class = PostSerializer
+	queryset = Post.objects.all()
+	
+	def get(self, request, *args, **kwargs):
+		return self.list(request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
+
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+	serializer_class = PostSerializer
+	queryset = Post.objects.all()
+
+	def get(self, request, *args, **kwargs):
+		return self.list(request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+	serializer_class = PostSerializer
+	queryset = Post.objects.all()
